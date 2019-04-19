bacdf8ddb1f28ad61616537b1b85306e592440bd
Yufei Lan <lanyufei518@gmail.com>
2019-04-19 21:46:25 +0000

    page styling updated

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,6 +3,28 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+// caculate the time difference between tweet post time and current time 
+function timeDiff(time1, time2) {
+  const msPerMinute = 60 * 1000;
+  const msPerHour = msPerMinute * 60;
+  const msPerDay = msPerHour * 24;
+  const msPerMonth = msPerDay * 30;
+  const msPerYear = msPerDay * 365;
+  let diff = time1 - time2;
+  if (diff < msPerMinute) {
+   return Math.round(diff/1000) + ' seconds ago';
+  } else if (diff < msPerHour) {
+   return Math.round(diff/msPerMinute) + ' minutes ago';
+  } else if (diff < msPerDay ) {
+   return Math.round(diff/msPerHour ) + ' hours ago';
+  } else if (diff < msPerMonth) {
+   return Math.round(diff/msPerDay) + ' days ago';
+  } else if (diff < msPerYear) {
+   return Math.round(diff/msPerMonth) + ' months ago';
+  } else {
+   return Math.round(diff/msPerYear ) + ' years ago';
+  }
+ }
 
 function escape(str) {
   var div = document.createElement('div');
@@ -29,16 +51,16 @@ $(document).ready(function() {
   });
 
   function createTweetElement (tweet) {
- 
+      const  tweetTime = timeDiff(Date.now(),tweet.created_at);
       let $tweet = $("<article>").addClass("tweet");
       let $header = $("<header>");
       let $profile = $("<img>").addClass("profile").attr("src", tweet.user.avatars.small);
       let $username = $("<h1>").addClass("username").text(tweet.user.name);
-      let $ID = $("<p>").addClass("ID").text(tweet.handle);
+      let $ID = $("<p>").addClass("ID").text(tweet.user.handle);
       let $content = $("<div>").addClass("content");
       let $text = $("<p>").addClass("post").text(tweet.content.text);
       let $footer = $("<footer>");
-      let $date = $("<p>").addClass("date").text(tweet.created_at);
+      let $date = $("<p>").addClass("date").text(tweetTime);
       let $iconContainer = $("<div>").addClass("icons");
       let $flag = $("<i>").addClass("fab fa-font-awesome-flag");
       let $retweet = $("<i>").addClass("fas fa-retweet");
@@ -59,11 +81,11 @@ $(document).ready(function() {
           event.preventDefault();
         
           if($('textarea').val() === ""){
-             return $( ".error" ).slideToggle().text("no tweet has been typed");
+             return $( ".error" ).slideDown().text("no tweet has been typed");
          
           }
           if ($('textarea').val().length > 140) {
-            return $( ".error" ).slideToggle().text("over 140 character limit");
+            return $( ".error" ).slideDown().text("over 140 character limit");
            
           }
 
@@ -74,7 +96,7 @@ $(document).ready(function() {
             complete: function(){
               $('textarea').val(null);
               loadTweets();
-              $(".error").slideToggle();
+              $(".error").slideUp();
             }
 
           });
--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -14,12 +14,12 @@ module.exports = function makeDataHelpers(db) {
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      db.collection("tweets").find().toArray(err,tweets) => {
+      db.collection("tweets").find().toArray((err,tweets) => {
         if(err) {
           return callback(err);
           }
         callback(null,tweets);
-      };
+      });
 
     }
 
